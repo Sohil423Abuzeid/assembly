0e1dcaa5f107c1852a7e653aa1d4146d23e400db
Saad Altohamy <92796871+saadaltohamy@users.noreply.github.com>
2023-12-26 13:25:09 +0200

    handling the input

--- QWF.asm
+++ QWF.asm
@@ -1,33 +1,34 @@
 .model small
 .data
-    fline DB 'welcome please enter binary numer max(10 digits)',0Ah,0Dh,'$'
-    error DB 'mot option sorry',0Ah,0Dh,'$'
-    sline DB 'convert it to ',0Ah,0Dh,'1-decimal',0Ah,0Dh,'2-octal',0Ah,0Dh,'3-hexa',0Ah,0Dh,'$'
-    bnum DB 11
+    welcome DB 'Welcome, please enter binary number max(10 digits)',0Ah,0Dh,'$'
+    error DB ' Not an obtion, sorry. Enter again: ',0Ah,0Dh,'$'
+    obtions DB 'convert it to ',0Ah,0Dh,'1-decimal',0Ah,0Dh,'2-octal',0Ah,0Dh,'3-hexa',0Ah,0Dh,'$'
+    bnum DB 11 DUP('$')
     endl Db 0Ah,0Dh
     
     dec_result db 10    
     dec_result_len dw 0
 .code
 MAIN PROC  
-    mov ax ,@data
-    mov ds, ax
-    
-    
-    mov ah ,09h
-    mov dx , offset fline
+    .startup
+
+    ; print welcome
+    MOV ah ,09h
+    LEA dx , welcome
     int 21h
     
-    
-     mov ah, 0Ah ; 
-     mov dx, offset bnum ;
-    ; int 21h ;
-   
-    
-    
-    mov ah ,09h
-    mov dx , offset sline
+    ; read string
+    MOV ah, 0Ah
+    LEA dx, bnum
+    INT 21h
+
+    ; print obtions
+obtion:
+    mov ah, 09h
+    LEA dx, obtions
     int 21h
+
+    ; read obtion and store it in al
     mov ah, 01h ; 
     int 21h ; 
     
@@ -40,10 +41,11 @@ MAIN PROC
     cmp al,'3'
     je hexadecimal
     
-    mov ah ,09h   ;error 
-    mov dx , offset error
+    ; print error if not 1 or 2 or 3, and jump to obtion
+    mov ah ,09h 
+    LEA dx , error
     int 21h
-    jmp endd
+    jmp obtion
     
     
     
@@ -61,6 +63,6 @@ hexadecimal:
     
       
 endd:
-.EXIT 
+    .EXIT 
 MAIN ENDP
 END MAIN
