17af0c9357a9783cc3fccbfec8f6edcba9a25193
Saad Altohamy <92796871+saadaltohamy@users.noreply.github.com>
2023-12-27 07:48:04 +0200

    Finish

--- QWF.asm
+++ QWF.asm
@@ -1,10 +1,13 @@
 .model small
 .STACK 300h
 .data
-    welcome DB 'Welcome, please enter binary number max(10 digits)',0Ah,0Dh,'Press e to end',0Ah,0Dh,'$'
+    welcome DB 'Welcome, please enter binary number max(10 digits)',0Ah,0Dh,'Press ENTER to end',0Ah,0Dh,'$'
     msg1 DB 0Ah,0Dh,'You entered: ',0Ah,0Dh,'$'
     error DB ' Not an option, sorry. Enter again: ',0Ah,0Dh,'$'
     obtions DB 0Ah,0Dh,0Ah,0Dh,'convert it to ',0Ah,0Dh,'1-decimal',0Ah,0Dh,'2-octal',0Ah,0Dh,'3-hexa',0Ah,0Dh, '9-End',0Ah,0Dh,'$'
+    deci_msg DB 0Ah,0Dh,'Decimal is: ',0Ah,0Dh,'$'
+    oct_msg DB 0Ah,0Dh,'Octal is: ',0Ah,0Dh,'$'
+    hex_msg DB 0Ah,0Dh,'Hexadecimal is: ',0Ah,0Dh,'$'
     bnum DB 11 DUP(?) ; buffer to store the binary number
     rbnum DB 11 DUP(?) ; buffer to store the reversed binary number
     fdnum DB 11 DUP(?) ; buffer to store the final decimal answer number
@@ -18,7 +21,7 @@
     oct_result db 5 dup (?) ; octal result will be stored here 
     dec_result_len db 0
 .code
-MAIN PROC  
+MAIN PROC FAR
     .startup
 
     ; print welcome
@@ -91,12 +94,15 @@ obtion:
     
     
     ;switch
-    cmp al,'1'
+    cmp al,'1' ; decimal obtion
     je decimal
-    cmp al,'2'
+    
+    cmp al,'2' ; octal obtion
     je octal
+    
     cmp al,'3'
-    je hexadecimal
+    je hex
+
     cmp al,'9'
     je endd
     
@@ -104,63 +110,133 @@ obtion:
     mov ah ,09h 
     LEA dx , error
     int 21h
-    jmp obtion
-    
+    jmp obtion 
+
 decimal:
+    call convert
+    call print_dec
+    jmp endd
+
+octal:
+    call convert
+    call print_oct
+    jmp endd
+
+hex: 
+    call convert
+    call print_hex
+    jmp endd
+
+endd:
+    .EXIT 
+MAIN ENDP
+
+convert PROC NEAR
     mov cx ,bx 
     xor bx ,bx ;reset to zero to store answer
     mov dx ,1  ;
     LEA di, rbnum
-cbd: ; covert binary to decimal loop
-    cmp [di],'0'
-    je novalue ;if current digit equal 0 skip
+conv_b_d: ; covert binary to decimal loop
+    cmp byte ptr [di], 30h
+    je no_value ;if current digit equal 0 skip
     add bx ,dx
-novalue:
+no_value:
     inc di
     shl dx, 1 ;multi dx by 2 
-    loop cbd
-    
-    ;;;;;;;;;;;;;;;;strore decimal in string 
-    LEA di, fdnum 
-    mov cx,0ah
-    xor ax,ax 
-    mov ax,bx 
-    ST : ; bx still bigger than 0  
-    xor dx,dx 
-    div cx
-    mov bx ,ax
-    add dl ,30h
-    mov [di],dl
-    inc di
-    mov ax,bx 
-    
-    test ax, ax    ; Check if quotient is zero
-    jnz ST ; If not, continue the loop
-     dec di
-     ;;;;;;;;;;;;;;;;;;;;
-         mov ah ,02h
-         LEA si, fdnum
- printt :
-     mov ah ,02h
-     mov dl , [di]
-     int 21h
-     dec di
-     cmp di,si
-     jne printt
-     mov dl , [di]
-     int 21
-    jmp endd
-    
-   
-    
-octal:
-    jmp endd
+    loop conv_b_d
+    ret
+convert ENDP
 
-hexadecimal:
+print_dec PROC NEAR
+    mov ax, bx
+    mov cx, 10
+    mov bx, 0
+    mov dx, 0
+    L1:
+        div cx
+        push dx
+        xor dx, dx
+        inc bx
+        cmp ax, 0
+        jne L1
 
-    jmp endd
+    lea dx, deci_msg
+    mov ah, 9
+    int 21h 
+    L2:
+        pop dx
+        add dl, 30h
+        mov ah, 2
+        int 21h
+        dec bx
+        cmp bx, 0
+        jne L2
+    ret
+print_dec ENDP
+
+print_oct PROC NEAR
+    mov ax, bx
+    mov cx, 8
+    mov bx, 0
+    mov dx, 0
+    L1_oct:
+        div cx
+        push dx
+        xor dx, dx
+        inc bx
+        cmp ax, 0
+        jne L1_oct
+
+    lea dx, oct_msg
+    mov ah, 9
+    int 21h 
+
+    L2_oct:
+        pop dx
+        add dl, 30h
+        mov ah, 2
+        int 21h
+        dec bx
+        cmp bx, 0
+        jne L2_oct
+    ret
+print_oct ENDP
+
+print_hex proc near
+    mov ax, bx
+    mov cx, 16
+    mov bx, 0
+    mov dx, 0
+    L1_hex:
+        div cx
+        push dx
+        xor dx, dx
+        inc bx
+        cmp ax, 0
+        jne L1_hex
     
-endd:
-    .EXIT 
-MAIN ENDP
+    lea dx, hex_msg
+    mov ah, 9
+    int 21h
+
+    L2_hex: ; loop for print the hex number if < 9
+        pop dx
+        cmp dx, 9
+        jg L3_hex
+        add dl, 30h
+        mov ah, 2
+        int 21h
+        dec bx
+        cmp bx, 0
+        jne L2_hex
+    L3_hex: ; loop for print the hex number charachter if > 9
+        add dl, 37h
+        mov ah, 2
+        int 21h
+        dec bx
+        cmp bx, 0
+        jne L2_hex
+    ret
+print_hex endp
+
 END MAIN
